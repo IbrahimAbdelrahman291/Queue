d5906a2079574353a370a65d8f30a2f50f0c6707
ebrah <ebrah@LAPTOP-TM8TPK8A>
2024-07-06 22:27:11 +0300

    Add project files.

---
+++ Queue/Program.cs
@@ -0,0 +1,80 @@
+﻿using Queue;
+using System.Collections;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        queue q = new queue();
+        int choice;
+        int value;
+        do 
+        {
+            ShowOptions();
+            Console.WriteLine("Enter your choice");
+            choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice) 
+            {
+                case 0:
+                    Console.WriteLine("Bye");
+                    break;
+                case 1:
+                    if (q.IsFull())
+                    {
+                        Console.WriteLine("queue is full");
+                    }
+                    else
+                    {
+                        Console.WriteLine("queue isn't full");
+                    }
+                    break;
+                case 2:
+                    if (q.IsEmpty())
+                    {
+                        Console.WriteLine("queue is empty");
+                    }
+                    else
+                    {
+                        Console.WriteLine("queue isn't empty");
+                    }
+                    break;
+                case 3:
+                    Console.WriteLine("Enter value");
+                    value = Convert.ToInt32(Console.ReadLine());
+                    q.Enqueue(value);
+                    break;
+                case 4:
+                    q.Dequeue();
+                    break;
+                case 5:
+                    if (q.peek() == 0)
+                    {
+                        Console.WriteLine("Already empty");
+                    }
+                    else
+                    {
+                        Console.WriteLine(q.peek());
+                    }
+                    break;
+                case 6:
+                    q.Display();
+                    break;
+                default:
+                    Console.WriteLine("Invaild option");
+                    break ;
+            }
+        }while (choice!=0);
+    }
+    public static void ShowOptions() 
+    {
+        Console.WriteLine("__________________________________");
+        Console.WriteLine("1-IsFull");
+        Console.WriteLine("2-IsEmpty");
+        Console.WriteLine("3-Enqueue");
+        Console.WriteLine("4-Dequeue");
+        Console.WriteLine("5-peek");
+        Console.WriteLine("6-Display");
+        Console.WriteLine("0-Exit");
+        Console.WriteLine("__________________________________");
+    }
+}
---
+++ Queue/queue.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+
+namespace Queue
+{
+    public class queue
+    {
+        int size;
+        int front;
+        int rear;
+        int[] q;
+        public queue() 
+        {
+            size = 10;
+            front = -1;
+            rear = -1;
+            q = new int[size];
+        }
+        public bool IsFull() 
+        {
+            return (rear == size-1) ? true : false;
+        }
+        public bool IsEmpty() 
+        {
+            return (front == -1 && rear == -1) ? true : false;
+        }
+        public int LengthOfQueue() 
+        {
+            return rear + 1;
+        }
+        public int peek() 
+        {
+            return (IsEmpty()) ? 0 : q[front];
+        }
+        public void Enqueue(int value) 
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Already full");
+            }
+            else if (IsEmpty()) 
+            {
+                front++;
+                rear++;
+                q[rear] = value;
+                Console.WriteLine("Item added successfully");
+            }
+            else
+            {
+                rear++;
+                q[rear] = value;
+                Console.WriteLine("Item added successfully");
+            }
+        }
+        public void Dequeue() 
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Already empty");
+            }
+            else if(front == rear)
+            {
+                q[front] = 0;
+                front--;
+                rear--;
+            }
+            else
+            {
+                q[front] = 0;
+                
+                for (int i = 0; i < rear; i++)
+                {
+                    q[i] = q[i + 1];
+                }
+                Console.WriteLine("Dequeue successfully");
+            }
+        }
+        public void Display() 
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("There are not elements to display it");
+            }
+            else 
+            {
+                for (int i = 0; i <= rear; i++)
+                {
+                    Console.Write($"{q[i]} <-");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
